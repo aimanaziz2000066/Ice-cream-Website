13d1c1c2420e43f1902e79f2fb35d460798edd13
aimanaziz <aimanaziz2000066@gmail.com>
2026-09-04 12:19:46 +0500

    Update images

--- main.js
+++ main.js
@@ -1,46 +1,62 @@
 var tl = gsap.timeline({
     scrollTrigger:{
-    trigger: "#s1",
-    start: "0% 80%",
-    end: "50% 50%",
-    scrub: true,
-}
+        trigger: "#s1",
+        start: "0% 80%",
+        end: "50% 50%",
+        scrub: true,
+    }
 })
+
 tl.to(".hero-center-image",{
     top: "123%",
     left: "50%",
-    width: "15vw",
+    width: "35vw",
     rotate: "0",
+
 })
 
+
 var tl1 = gsap.timeline({
+
     scrollTrigger:{
-    trigger: "#s2",
-    start: "0% 65%",
-    end: "50% 50%",
-    scrub: true,
-}
+
+        trigger: "#s2",
+
+        start: "0% 65%",
+
+        end: "50% 50%",
+
+        scrub: true,
+
+    }
+
 })
+
+
 tl1.to(".hero-center-image",{
     top: "190%",
     left: "21.5%",
-    width: "14.5vw",
+    width: "35.8vw",
     rotate: "0",
+
 })
 
+
 var tl2 = gsap.timeline({
+
     scrollTrigger:{
-    trigger: "#s3",
-    start: "0% 80%",
-    end: "50% 50%",
-    scrub: true,
-}
+        trigger: "#s3",
+        start: "0% 80%",
+        end: "50% 50%",
+        scrub: true,
+    }
 })
+
+
 tl2.to(".hero-center-image",{
     top: "270.5%",
     left: "30%",
-    width: "14.5vw",
+    width: "30.2vw",
     rotate: "-50deg",
+
 })
-
-
